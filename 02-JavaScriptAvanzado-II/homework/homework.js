4f7f7c459c31ea65b41b577380a4e8572bbb3846
Rober-T23 <roberbaldovino167@gmail.com>
2022-03-31 00:46:24 -0500

    avance 2

--- 02-JavaScriptAvanzado-II/homework/homework.js
+++ 02-JavaScriptAvanzado-II/homework/homework.js
@@ -1,5 +1,6 @@
 'use strict'
 
+
 function counter() {
   // Retorna una funcion que cuando sea invocada retorne un valor creciente.
   // el primer valor deberia ser 1.
@@ -7,8 +8,19 @@ function counter() {
   // ejemplo: const newCounter = counter();
   // newCounter(); // 1
   // newCounter(); // 2
+  var num=1;
+  return function (){
+     
+      return num++;
+      
+  }
+ 
+ 
 }
+const newCounter = counter();
 
+newCounter();
+newCounter();
 function cacheFunction(cb) {
   // Usa closures para crear un caché para la función cb.
   // la función que retornas debe aceptar un solo argumento e invocar a cb con ese argumento
@@ -21,6 +33,16 @@ function cacheFunction(cb) {
   // si la invocas de nuevo con 5, deberia retornar 25 (guardado previament en el cache)
   // Tips, usá un objeto donde cada propiedad sea un argumento, y el valor el resultado.
   // usá hasOwnProperty!
+  var argumento= {}
+  return function(cs){
+    if (argumento.hasOwnProperty(cs)){
+         return argumento[cs];
+    }else{
+       argumento[cs]= cb(cs);
+       return argumento[cs];
+    }
+  }
+     
 }
 
 // Bind
@@ -41,9 +63,10 @@ function getNombre(){
  // Escribir código, sin modificar lo que ya se encuentra escrito arriba, para poder llamar al método getNombre para obtener primero el nombre del instructor y luego para obtener el nombre del alumno.
 // Modificar los undefined por el código correspondiente en cada caso
 // Pista, tenes que bindear el this!
-let getNombreInstructor = undefined;
-let getNombreAlumno = undefined;
-
+let getNombreInstructor = getNombre.bind(instructor);
+let getNombreAlumno = getNombre.bind(alumno);
+getNombreAlumno();
+getNombreInstructor();
 
 /*Guardar en las siguientes tres variables una función que devuelva una cadena utilizando la función "crearCadena"
 y el delimitador especificado. La idea es realizarlo con la función bind para poder volver a utilizarlo múltiples veces luego:
@@ -61,12 +84,14 @@ function crearCadena(delimitadorIzquierda, delimitadorDerecha, cadena){
 // Modificar los undefined por el código correspondiente en cada caso
 // Pista, tenes que usar bind para "bindear" algunos parámetros de la función crearCadena.
 
-let textoAsteriscos = undefined;
+let textoAsteriscos = crearCadena.bind("","*","*");
 
-let textoGuiones = undefined;
+let textoGuiones = crearCadena.bind("","-","-");
 
-let textoUnderscore = undefined;
-
+let textoUnderscore = crearCadena.bind("","_","_");
+textoAsteriscos();
+textoGuiones();
+textoUnderscore();
 
 
 // No modifiquen nada debajo de esta linea
